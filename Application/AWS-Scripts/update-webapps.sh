44bde5858d852e35781e2436fb2c5e004366cef0
John James <96994184+johnjamesktm@users.noreply.github.com>
2023-06-13 18:14:32 -0400

    Update scripts

--- Application/AWS-Scripts/update-webapps.sh
+++ Application/AWS-Scripts/update-webapps.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 cd /home/ec2-user/github/Project-4
 git pull
-cp heart_app.py /home/ec2-user/webapps/heart_app.py
-cp heart_app.html /home/ec2-user/webapps/heart_app.html
+cp Application/heart_app.py /home/ec2-user/webapps/heart_app.py
+cp Application/heart_app.html /home/ec2-user/webapps/heart_app.html
